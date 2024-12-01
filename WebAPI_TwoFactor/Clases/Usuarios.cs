28d2dc581743cf3883123196241b6e30d2e3340f
Nathanael Martinez <kevin_1321124060@uptecamac.edu.mx>
2024-11-30 20:32:23 -0600

    First commit

---
+++ WebAPI_TwoFactor/Clases/Usuarios.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+
+namespace WebAPI_TwoFactor.Clases
+{
+    public class Usuarios
+    {
+
+        public Usuarios()
+        {
+            AccesoDatos.cadenaConexion = @"Data Source=NATHANAEL\MSSQLSERVER01;Initial Catalog=2FA;Integrated Security=SSPI;";
+        }
+
+        public void SetSecret(string email, string code)
+        {
+            AccesoDatos.ExecuteQuery($"UPDATE dbo.Usuarios SET TwoFactorSecret='{code}' WHERE email='{email}'");
+        }
+
+        public string GetSecret(string email)
+        {
+            DataTable dt = AccesoDatos.GetTmpDataTable($"SELECT TwoFactorSecret FROM dbo.Usuarios WHERE email='{email}'");
+            if (dt.Rows.Count == 1)
+            {
+                return dt.Rows[0]["TwoFactorSecret"].ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+    }
+}
---
+++ WebAPI_TwoFactor/Controllers/TwoFactorController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TwoFactorAuthNet;
+using TwoFactorAuthNet.Providers.Qr;
+using WebAPI_TwoFactor.Clases;
+
+namespace WebAPI_TwoFactor.Controllers
+{
+    public class TwoFactorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet, Route("GetQRCode")]
+        public string GetQRCode(string email)
+        {
+            var tfa = new TwoFactorAuth("Example", 6, 30, Algorithm.SHA256, new ImageChartsQrCodeProvider());
+            var secret = tfa.CreateSecret(160);
+
+            Usuarios usu = new Usuarios();
+            usu.SetSecret(email, secret);
+            usu = null;
+
+            string imgQR = tfa.GetQrCodeImageAsDataUri(email, secret);
+            string imgHTML = $"<img src='{imgQR}'>";
+            return imgHTML;
+        }
+
+        
+
+        [HttpGet, Route("ValidarQRCode")]
+        public bool ValidarCodigo(string email, string code)
+        {
+            Usuarios usu = new Usuarios();
+            string secret = usu.GetSecret(email);
+            usu = null;
+
+            var tfa = new TwoFactorAuth("ManuelToscanoDEV", 6, 30, Algorithm.SHA256);
+            return tfa.VerifyCode(secret, code);
+        }
+    }
+}
